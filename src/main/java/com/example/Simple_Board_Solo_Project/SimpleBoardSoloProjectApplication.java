e9a8432a65139ed79c58a988929430055ebce479
Tizesin <tlsalswns0o@gmail.com>
2024-07-10 18:52:23 +0900

    초기설정

---
+++ src/main/java/com/example/Simple_Board_Solo_Project/SimpleBoardSoloProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Simple_Board_Solo_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBoardSoloProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBoardSoloProjectApplication.class, args);
+	}
+
+}
